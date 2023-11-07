4a64d027c89d6fd5493329c375bcd8fddca2e347
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-07 16:15:40 +0530

    eepy

--- src/commenter.py
+++ src/commenter.py
@@ -1,6 +1,6 @@
 import glob
-import torch
 import os
+import torch
 from transformers import AutoModel, AutoTokenizer
 
 checkpoint = "Salesforce/codet5p-220m-bimodal"
@@ -10,7 +10,7 @@ tokenizer = AutoTokenizer.from_pretrained(checkpoint, trust_remote_code=True)
 model = AutoModel.from_pretrained(checkpoint, trust_remote_code=True).to(device)
 
 # Get the path to the src directory
-src_path = os.path.join(os.path.dirname(__file__), "src")
+src_path = os.path.join("${{ github.workspace }}", "src")
 
 # Find all Python files in the src directory
 python_files = glob.glob(os.path.join(src_path, "*.py"))
@@ -21,11 +21,11 @@ for file in python_files:
   with open(file, "r") as f:
     code_string = f.read()
 
-    input_ids = tokenizer(code_string, return_tensors="pt").input_ids.to(device)
+  input_ids = tokenizer(code_string, return_tensors="pt").input_ids.to(device)
 
-    generated_ids = model.generate(input_ids, max_length=20)
-    tokens = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
-    print(tokens)
-    # Write the output to the file
-    with open("src/files/output.txt", "a") as f:
-      f.write(f'{tokens}\n')
+  generated_ids = model.generate(input_ids, max_length=20)
+  tokens = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+  print(tokens)
+  # Write the output to the file
+  with open("src/files/output.txt", "a") as f:
+    f.write(f'{tokens}\n')
