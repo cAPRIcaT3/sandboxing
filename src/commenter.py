06074d3c0c21e95fa20101b6c009ab8bc6c8069d
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-07 16:30:01 +0530

    serious boi

--- src/commenter.py
+++ src/commenter.py
@@ -12,20 +12,29 @@ model = AutoModel.from_pretrained(checkpoint, trust_remote_code=True).to(device)
 # Get the path to the src directory
 src_path = os.path.join("${{ github.workspace }}", "src")
 
-# Find all Python files in the src directory
-python_files = glob.glob(os.path.join(src_path, "*.py"))
+# Find all files in the src directory
+all_files = glob.glob(os.path.join(src_path, "*"))
 
-print(python_files)
-for file in python_files:
-  code_string = ""
-  with open(file, "r") as f:
-    code_string = f.read()
+# Loop through all files in the src directory
+for file in all_files:
 
-  input_ids = tokenizer(code_string, return_tensors="pt").input_ids.to(device)
+  # Check if the file is a regular file
+  if os.path.isfile(file):
 
-  generated_ids = model.generate(input_ids, max_length=20)
-  tokens = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
-  print(tokens)
-  # Write the output to the file
-  with open("src/files/output.txt", "a") as f:
-    f.write(f'{tokens}\n')
+    # Open the file and read the code
+    with open(file, "r") as f:
+      code = f.read()
+
+    # Encode the code into input IDs
+    input_ids = tokenizer(code, return_tensors="pt").input_ids.to(device)
+
+    # Generate a comment for the code
+    generated_ids = model.generate(input_ids, max_length=20)
+    tokens = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+
+    # Print the comment
+    print(tokens)
+
+    # Write the comment to the output file
+    with open("src/files/output.txt", "a") as f:
+      f.write(f"{tokens}\n")
