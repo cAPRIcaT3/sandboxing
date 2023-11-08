be374731fabe7686331c024c9ee13b990cf7a379
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-08 15:33:51 +0530

    using gpt-neo

--- src/generate_comment.py
+++ src/generate_comment.py
@@ -1,13 +1,18 @@
 import glob
 import os
 import torch
-from transformers import AutoModel, AutoTokenizer
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
 
-checkpoint = "Salesforce/codet5p-220m-bimodal"
+# Update the checkpoint for GPT-Neo
+checkpoint = "EleutherAI/gpt-neo-2.7B"
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-tokenizer = AutoTokenizer.from_pretrained(checkpoint, trust_remote_code=True)
-model = AutoModel.from_pretrained(checkpoint, trust_remote_code=True).to(device)
+# Add variables for prompts
+prompt_start = "This is a piece of code for review: "
+prompt_end = "Review complete. Comments: "
+
+tokenizer = GPT2Tokenizer.from_pretrained(checkpoint)
+model = GPT2LMHeadModel.from_pretrained(checkpoint).to(device)
 
 # Get the path to the src directory
 src_path = os.path.join(os.getenv("GITHUB_WORKSPACE"), "src")
@@ -19,28 +24,29 @@ all_files = glob.glob(os.path.join(src_path, "*"))
 # Loop through all files in the src directory
 for file in all_files:
 
-  # Check if the file is a regular file
-  if os.path.isfile(file):
+    # Check if the file is a regular file
+    if os.path.isfile(file):
 
-    # Print the file name
-    print(f"Processing file: {file}")
+        # Print the file name
+        print(f"Processing file: {file}")
 
-    # Open the file and read the code
-    with open(file, "r") as f:
-      code = f.read()
+        # Open the file and read the code
+        with open(file, "r") as f:
+            code = f.read()
 
-    # Encode the code into input IDs
-    input_ids = tokenizer(code, return_tensors="pt").input_ids.to(device)
+        # Construct full prompt
+        full_prompt = f"{prompt_start}\n{code}\n{prompt_end}\n"
 
-    # Generate a comment for the code
-    generated_ids = model.generate(input_ids, max_length=20)
-    tokens = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
+        # Tokenize the prompt
+        input_ids = tokenizer.encode(full_prompt, return_tensors="pt").to(device)
 
-    # Print the comment
-    print(f"Generated comment: {tokens}")
+        # Generate a comment for the code
+        generated_ids = model.generate(input_ids, max_length=100, num_return_sequences=1)
+        tokens = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
 
-    # Write the comment to the output file
-    with open("src/files/output.txt", "a") as f:
-      f.write(f"{tokens}\n")
-      f.write("\n")
-      f.write("\n")
+        # Print the comment
+        print(f"Generated comment: {tokens}")
+
+        # Write the comment to the output file
+        with open("src/files/output.txt", "a") as f:
+            f.write(f"{tokens}\n\n\n")
