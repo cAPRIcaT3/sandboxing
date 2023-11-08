c001c7d4c6bf014f552e48528dbfdb3ad2b391de
Arpit Dwivedi <58943465+cAPRIcaT3@users.noreply.github.com>
2023-11-08 15:44:32 +0530

    Update generate_comment.py

--- src/generate_comment.py
+++ src/generate_comment.py
@@ -1,7 +1,7 @@
 import glob
 import os
 import torch
-from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from transformers import GPTNeoForCausalLM, GPT2Tokenizer
 
 # Update the checkpoint for GPT-Neo
 checkpoint = "EleutherAI/gpt-neo-2.7B"
@@ -12,41 +12,34 @@ prompt_start = "This is a piece of code for review: "
 prompt_end = "Review complete. Comments: "
 
 tokenizer = GPT2Tokenizer.from_pretrained(checkpoint)
-model = GPT2LMHeadModel.from_pretrained(checkpoint).to(device)
+model = GPTNeoForCausalLM.from_pretrained(checkpoint).to(device)
 
 # Get the path to the src directory
 src_path = os.path.join(os.getenv("GITHUB_WORKSPACE"), "src")
 src_path = os.path.expandvars(src_path)
 
-# Find all files in the src directory
-all_files = glob.glob(os.path.join(src_path, "*"))
+# Define the output directory
+output_dir = os.path.join(src_path, "files")
+os.makedirs(output_dir, exist_ok=True)  # Create the directory if it doesn't exist
 
-# Loop through all files in the src directory
-for file in all_files:
+output_file = os.path.join(output_dir, "output.txt")
 
-    # Check if the file is a regular file
-    if os.path.isfile(file):
+# Open the output file outside the loop
+with open(output_file, "a") as output:
+    # Loop through all files in the src directory
+    for file in glob.glob(os.path.join(src_path, "*")):
+        if os.path.isfile(file):
+            print(f"Processing file: {file}")
+            with open(file, "r") as f:
+                code = f.read()
 
-        # Print the file name
-        print(f"Processing file: {file}")
+            full_prompt = f"{prompt_start}\n{code}\n{prompt_end}\n"
+            input_ids = tokenizer.encode(full_prompt, return_tensors="pt").to(device)
 
-        # Open the file and read the code
-        with open(file, "r") as f:
-            code = f.read()
+            generated_ids = model.generate(input_ids, max_length=100, num_return_sequences=1)
+            tokens = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
 
-        # Construct full prompt
-        full_prompt = f"{prompt_start}\n{code}\n{prompt_end}\n"
+            print(f"Generated comment: {tokens}")
 
-        # Tokenize the prompt
-        input_ids = tokenizer.encode(full_prompt, return_tensors="pt").to(device)
-
-        # Generate a comment for the code
-        generated_ids = model.generate(input_ids, max_length=100, num_return_sequences=1)
-        tokens = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
-
-        # Print the comment
-        print(f"Generated comment: {tokens}")
-
-        # Write the comment to the output file
-        with open("src/files/output.txt", "a") as f:
-            f.write(f"{tokens}\n\n\n")
+            # Write the comment to the output file
+            output.write(f"{tokens}\n\n\n")
